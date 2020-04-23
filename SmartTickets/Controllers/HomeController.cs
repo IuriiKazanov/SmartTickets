437245616b3bb39a6bf8dd4b14312012cf75027b
ua.kazanov <kazanov.98@mail.ru>
2020-04-23 16:11:13 +0300

    Структура приложения

---
+++ SmartTickets/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FNL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BuyTickets()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeTickets()
+        {
+            ViewBag.Message = "Change tickets";
+
+            return View();
+        }
+
+        public ActionResult MyTickets()
+        {
+            ViewBag.Message = "Your tickets";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SmartTickets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartTickets.Startup))]
+namespace SmartTickets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
